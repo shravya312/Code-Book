6ebae69e870b4425cbc34483ffb68dbf8a2df8e3
shravya312 <shravyahjainjain@gmail.com>
2024-10-04 19:50:27 +0530

    added youtube links

--- src/components/CourseCards.jsx
+++ src/components/CourseCards.jsx
@@ -22,22 +22,118 @@ function CourseCards() {
     <>
       <div className="course-page" id="course-page">
         <div className="courses">
-          <Card imgurl={java} coursename={"Java"} authorname={"Kunal Kushwaha"} lessons={65} ytlink={"https://www.youtube.com/@kunalkushwaha"} />
-          <Card imgurl={c} coursename={"C"} authorname={"Sandeep Jain"} lessons={50} ytlink={"https://www.youtube.com/@sandeepjain"} />
-          <Card imgurl={CPP} coursename={"CPP"} authorname={"Gaurav Sen"} lessons={45} ytlink={"https://www.youtube.com/@gkcs"} />
-          <Card imgurl={Chash} coursename={"C#"} authorname={"Anuj Bhaiya"} lessons={40} ytlink={"https://www.youtube.com/@AnujBhaiya"} />
-          <Card imgurl={Ruby} coursename={"Ruby"} authorname={"Bhavin Patel"} lessons={55} ytlink={""} />
-          <Card imgurl={python} coursename={"Python"} authorname={"Tanay Pratap"} lessons={70} ytlink={"https://www.youtube.com/@tanaypratap"} />
-          <Card imgurl={CSS} coursename={"CSS"} authorname={"Prateek Narang"} lessons={60} ytlink={""} />
-          <Card imgurl={go} coursename={"Go"} authorname={"Akash Thakur"} lessons={35} ytlink={""} />
-          <Card imgurl={HTML} coursename={"HTML"} authorname={"Vishal Kumar"} lessons={25} ytlink={"https://www.youtube.com/@tbhvishalkumar"} />
-          <Card imgurl={JS} coursename={"Javascript"} authorname={"Nikhil Malankar"} lessons={55} ytlink={"https://www.youtube.com/@nikmlnkr"} />
-          <Card imgurl={kotlin} coursename={"Kotlin"} authorname={"Sanket Singh"} lessons={42} ytlink={"https://www.youtube.com/@SanketSingh1"} />
-          <Card imgurl={node} coursename={"Node.js"} authorname={"Hitesh Choudhary"} lessons={50} ytlink={"https://www.youtube.com/@HiteshCodeLab"} />
-          <Card imgurl={swift} coursename={"Swift"} authorname={"Kunal Kushwaha"} lessons={65} ytlink={"https://www.youtube.com/@kunalkushwaha"} />
-          <Card imgurl={php} coursename={"PHP"} authorname={"Anurag Tiwari"} lessons={65} ytlink={""} />
-          <Card imgurl={react} coursename={"React"} authorname={"Amman Dhattarwal"} lessons={65} ytlink={"https://www.youtube.com/@AmanDhattarwal"} />
-          <Card imgurl={next} coursename={"Next.js"} authorname={"College Wallah"} lessons={25} ytlink={"https://www.youtube.com/@CollegeWallahbyPW"} />
+          <Card 
+            imgurl={java} 
+            coursename={"Java"} 
+            authorname={"Kunal Kushwaha"} 
+            lessons={65} 
+            ytlink={"https://www.youtube.com/@kunalkushwaha"} 
+          />
+          <Card 
+            imgurl={c} 
+            coursename={"C"} 
+            authorname={"Sandeep Jain"} 
+            lessons={50} 
+            ytlink={"https://www.youtube.com/@sandeepjain"} 
+          />
+          <Card 
+            imgurl={CPP} 
+            coursename={"CPP"} 
+            authorname={"Gaurav Sen"} 
+            lessons={45} 
+            ytlink={"https://www.youtube.com/@gkcs"} 
+          />
+          <Card 
+            imgurl={Chash} 
+            coursename={"C#"} 
+            authorname={"Anuj Bhaiya"} 
+            lessons={40} 
+            ytlink={"https://www.youtube.com/@AnujBhaiya"} 
+          />
+          <Card 
+            imgurl={Ruby} 
+            coursename={"Ruby"} 
+            authorname={"Bhavin Patel"} 
+            lessons={55} 
+            ytlink={""} 
+          />
+          <Card 
+            imgurl={python} 
+            coursename={"Python"} 
+            authorname={"Tanay Pratap"} 
+            lessons={70} 
+            ytlink={"https://www.youtube.com/@tanaypratap"} 
+          />
+          <Card 
+            imgurl={CSS} 
+            coursename={"CSS"} 
+            authorname={"Prateek Narang"} 
+            lessons={60} 
+            ytlink={""} 
+          />
+          <Card 
+            imgurl={go} 
+            coursename={"Go"} 
+            authorname={"Akash Thakur"} 
+            lessons={35} 
+            ytlink={""} 
+          />
+          <Card 
+            imgurl={HTML} 
+            coursename={"HTML"} 
+            authorname={"Vishal Kumar"} 
+            lessons={25} 
+            ytlink={"https://www.youtube.com/@tbhvishalkumar"} 
+          />
+          <Card 
+            imgurl={JS} 
+            coursename={"Javascript"} 
+            authorname={"Nikhil Malankar"} 
+            lessons={55} 
+            ytlink={"https://www.youtube.com/@nikmlnkr"} 
+          />
+          <Card 
+            imgurl={kotlin} 
+            coursename={"Kotlin"} 
+            authorname={"Sanket Singh"} 
+            lessons={42} 
+            ytlink={"https://www.youtube.com/@SanketSingh1"} 
+          />
+          <Card 
+            imgurl={node} 
+            coursename={"Node.js"} 
+            authorname={"Hitesh Choudhary"} 
+            lessons={50} 
+            ytlink={"https://www.youtube.com/@HiteshCodeLab"} 
+          />
+          <Card 
+            imgurl={swift} 
+            coursename={"Swift"} 
+            authorname={"Kunal Kushwaha"} 
+            lessons={65} 
+            ytlink={"https://www.youtube.com/@kunalkushwaha"} 
+          />
+          <Card 
+            imgurl={php} 
+            coursename={"PHP"} 
+            authorname={"Anurag Tiwari"} 
+            lessons={65} 
+            ytlink={""} 
+          />
+          <Card 
+            imgurl={react} 
+            coursename={"React"} 
+            authorname={"Aman Dhattarwal"} 
+            lessons={65} 
+            ytlink={"https://www.youtube.com/@AmanDhattarwal"} 
+          />
+          <Card 
+            imgurl={next} 
+            coursename={"Next.js"} 
+            authorname={"College Wallah"} 
+            lessons={25} 
+            ytlink={"https://www.youtube.com/@CollegeWallahbyPW"} 
+          />
         </div>
       </div>
     </>
